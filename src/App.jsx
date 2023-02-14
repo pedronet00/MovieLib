5aaa1a7d711e2167b120531ae4fab7f87ee951af
Pedro Neto <stabilepedro010403@gmail.com>
2023-02-14 11:29:10 -0300

    feito a tela de visualização de filmes

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Link, Outlet } from 'react-router-dom'
+import Navbar from "./components/Navbar";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Outlet />
+    </div>
+  )
+}
+
+export default App
